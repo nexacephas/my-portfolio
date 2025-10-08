48df82912d8743d7b0ab7e469bb9c8abde61dae5
cephas <preciousayo773@gmail.com>
2025-10-08 06:56:44 +0100

    Fix image paths for Project-3 to Project-6 in Portfolio component

--- src/components/Portfolio/Portfoilo.jsx
+++ src/components/Portfolio/Portfoilo.jsx
@@ -3,9 +3,9 @@ import './Portfoilo.css';
 import { FaExternalLinkAlt, FaGithub } from 'react-icons/fa';
 import project_1 from '../../assets/Project-1.png';
 import project_2 from '../../assets/project-2.png'; 
-import project_3 from '../../assets/Project-3.png';
-import project_4 from '../../assets/Project-4.png';
-import project_5 from '../../assets/Project-5.png';
+import project_3 from '../../assets/project-3.png';
+import project_4 from '../../assets/project-4.png';
+import project_5 from '../../assets/project-5.png';
 import project_6 from '../../assets/project-6.jpg';
 
 const portfolioProjects = [
